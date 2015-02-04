5a804e534795b30813bbf031c41762b1321b958c
Joseph Tingsanchali <charnwoot@gmail.com>
2015-02-03 21:23:14 -0600

    Accept string input

--- program.c
+++ program.c
@@ -6,3 +6,19 @@ int is_valid(const char* password)
     return 0;
   }
 }
+
+int main()
+{
+  char* input = malloc(256);
+  printf("Please input a word: ");
+  scanf("%s", input);
+
+  if (is_valid(input)) {
+    printf("That's correct!\n");
+  } else {
+    printf("That's not correct!\n");
+  }
+
+  free(input);
+  return 0;
+}
